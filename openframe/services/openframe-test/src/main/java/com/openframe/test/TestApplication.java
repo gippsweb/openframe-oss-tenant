47332cb15ae9a3d7760025f68dc80a22561775ee
gio <122273656+giokur@users.noreply.github.com>
2026-03-03 23:41:03 +0100

    update logging (#1572)

--- openframe/services/openframe-test/src/main/java/com/openframe/test/TestApplication.java
+++ openframe/services/openframe-test/src/main/java/com/openframe/test/TestApplication.java
@@ -1,28 +1,31 @@
 package com.openframe.test;
 
+import com.openframe.test.listener.SlackListener;
 import com.openframe.test.runner.TestRunner;
 import com.openframe.test.runner.TestRunnerConfig;
 import io.qameta.allure.junitplatform.AllureJunitPlatform;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
 import org.junit.platform.launcher.TestPlan;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
 
 import static com.openframe.test.SummaryLogger.logSummary;
 import static com.openframe.test.SummaryLogger.logTestList;
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPackage;
+import static org.junit.platform.launcher.TagFilter.includeTags;
 
 @Slf4j
 public class TestApplication {
     private static final String TEST_PACKAGE = "com.openframe.test.tests";
 
     public static void main(String[] args) {
-//        io.restassured.RestAssured.filters(new AllureRestAssured(), new RequestLoggingFilter());
-
         AllureJunitPlatform allureListener = new AllureJunitPlatform();
         SummaryGeneratingListener summaryListener = new SummaryGeneratingListener();
-
+        SlackListener slackListener = new SlackListener(System.getenv("SLACK_HOOK"));
         TestRunnerConfig config = TestRunnerConfig.builder()
                 .testPackage(TEST_PACKAGE)
-                .testListeners(allureListener, summaryListener)
+                .testListeners(allureListener, summaryListener, slackListener)
                 .build();
 
         TestRunner testRunner = new TestRunner(config);
@@ -39,14 +42,29 @@ public class TestApplication {
         }
         if (testsPassed) {
             log.info("Run OSS tests");
-            TestPlan testPlan = testRunner.discover("oss");
+            LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+                    .selectors(selectPackage(config.getTestPackage()))
+                    .filters(includeTags("oss"))
+                    .build();
+            TestPlan testPlan = testRunner.discover(request);
             logTestList(testRunner.list(testPlan));
             testRunner.run(testPlan);
             logSummary(summaryListener.getSummary());
             testsPassed = summaryListener.getSummary().getTestsFailedCount() == 0;
+
+//            request = LauncherDiscoveryRequestBuilder.request()
+//                    .selectors(selectPackage(config.getTestPackage()))
+//                    .filters(includeClassNamePatterns(".*ResetPasswordTest"))
+//                    .build();
+//            testPlan = testRunner.discover(request);
+//            logTestList(testRunner.list(testPlan));
+//            testRunner.run(testPlan);
+//            logSummary(summaryListener.getSummary());
+//            testsPassed = testsPassed && summaryListener.getSummary().getTestsFailedCount() == 0;
         } else {
             log.error("Registration tests failed - interrupting execution");
         }
+        slackListener.sendResults("oss", "localhost", "");
         System.exit(testsPassed ? 0 : 1);
     }
 }
